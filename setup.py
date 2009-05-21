029bc6cc5ee606eca86aa4218ffcf907f8ccaee8
Matt Pizzimenti <mjpizz@gmail.com>
2009-05-21 10:11:02 -0400

    added initial codebase

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+
+setup(
+    name="rython",
+    packages=["rython"],
+    version="0.0.1",
+    license="BSD",
+    author="Matt Pizzimenti",
+    author_email="mjpizz+rython@gmail.com",
+    url="http://pypi.python.org/pypi/rython/",
+    # install_requires=[],
+    # install_recommends=["PyYAML"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Ruby",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Object Brokering",
+        "Topic :: Software Development :: Libraries :: Ruby Modules",
+    ],
+    description="rython transparently mixes Ruby code into Python",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read(),
+    # zip_safe=False,
+    # cmdclass=cmdclass,
+    # ext_modules=ext_modules,
+    )
